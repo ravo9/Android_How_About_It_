9f30e4b5c8cc84f088f65ab320ca60b4471b609d
Rafal Ozog <rafalozog@gmail.com>
2020-02-16 13:12:05 +0000

    Comment added

--- app/src/main/java/dreamcatcher/howaboutit/network/ApiClient.kt
+++ app/src/main/java/dreamcatcher/howaboutit/network/ApiClient.kt
@@ -6,6 +6,7 @@ import retrofit2.http.GET
 // External gate for communication with API endpoints (operated by Retrofit)
 interface ApiClient {
 
+    // CHANGE!!
     //@GET("/uc?authuser=0&id=16-nnDpeAozVo6B5CINlF0peLkw5uoXSC&export=download")
     @GET("/uc?id=1mXzTtXYK5ezl9p-7zuKbhEDOZAMkQuG_&export=download")
     fun getItems(): Observable<List<ItemPojo>>
