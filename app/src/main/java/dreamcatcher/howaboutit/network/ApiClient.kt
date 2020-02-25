5e17667e4676c815003647a44659594d6d38ff77
Rafal Ozog <rafalozog@gmail.com>
2020-02-25 20:51:17 +0000

    A proper link form implemented.

--- app/src/main/java/dreamcatcher/howaboutit/network/ApiClient.kt
+++ app/src/main/java/dreamcatcher/howaboutit/network/ApiClient.kt
@@ -6,9 +6,9 @@ import retrofit2.http.GET
 // External gate for communication with API endpoints (operated by Retrofit)
 interface ApiClient {
 
-    @GET("/uc?authuser=0&id=16-nnDpeAozVo6B5CINlF0peLkw5uoXSC&export=download")
+    @GET(Constants.OLD_DATABASE_URL)
     fun getItems(): Observable<List<ItemPojo>>
 
-    @GET("/uc?authuser=0&id=1UO15vtyfnx4Ld29JRDzXaj7XYpnCUV1A&export=download")
+    @GET(Constants.PROTIPS_URL)
     fun getProtips(): Observable<List<ProtipPojo>>
 }
--- app/src/main/java/dreamcatcher/howaboutit/network/Constants.kt
+++ app/src/main/java/dreamcatcher/howaboutit/network/Constants.kt
@@ -6,5 +6,12 @@ class Constants {
 
         const val BASE_URL = "https://drive.google.com"
 
+        const val TESTING_DATABASE_URL = "/uc?id=1mXzTtXYK5ezl9p-7zuKbhEDOZAMkQuG_&export=download"
+
+        const val OLD_DATABASE_URL = "/uc?authuser=0&id=16-nnDpeAozVo6B5CINlF0peLkw5uoXSC&export=download"
+
+        const val NEW_DATABASE_URL = "/uc?authuser=0&id=1ur2MvOuJAzm8U28j37KyNUVQ-5Yw0b4H&export=download"
+
+        const val PROTIPS_URL = "/uc?authuser=0&id=1UO15vtyfnx4Ld29JRDzXaj7XYpnCUV1A&export=download"
     }
 }
